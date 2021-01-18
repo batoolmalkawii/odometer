203ca790211b0655cf60016cc7137b52e5c022e6
Batool Malkawi <batoolmalkawii@gmail.com>
2021-01-18 16:48:11 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function Header(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="Header ">
+      <p>{props.text}</p>
+    </header>
   );
 }
+
+function Footer(props) {
+  return (
+    <footer className="Footer">
+      <p>Copyrights reserved to {props.trademark}</p>
+    </footer>
+  );
+}
+
+class Odometer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      odometer: 0
+    }
+  }
+  
+  plus_1() {
+    if (this.state.odometer + 1 > 9999) {
+      this.setState(() => ({
+        odometer: this.state.odometer + 1 - 9999
+      }));
+    }
+    else {
+      this.setState(() => ({
+        odometer: this.state.odometer + 1
+      }));
+    }
+  }
+
+  plus_10() {
+    if (this.state.odometer + 10 > 9999) {
+      this.setState(() => ({
+        odometer: this.state.odometer + 10 - 9999
+      }));
+    }
+    else {
+      this.setState(() => ({
+        odometer: this.state.odometer + 10
+      }));
+    }
+  }
+
+  plus_100() {
+    if (this.state.odometer + 100 > 9999) {
+      this.setState(() => ({
+        odometer: this.state.odometer + 100 - 9999
+      }));
+    }
+    else {
+      this.setState(() => ({
+        odometer: this.state.odometer + 100
+      }));
+    }
+  }
+
+  plus_1000() {
+    if (this.state.odometer + 1000 > 9999) {
+      this.setState(() => ({
+        odometer: this.state.odometer + 1000 - 9999
+      }));
+    }
+    else {
+      this.setState(() => ({
+        odometer: this.state.odometer + 1000
+      }));
+    }
+  }
+
+  reset() {
+    this.setState(() => ({
+      odometer: 0
+    }));
+  }
+
+  render() {
+    console.log(this.state.odometer)
+    return (
+      <>
+        <h1>{this.state.odometer}</h1>
+
+        <button onClick={() => this.plus_1()}>+1</button>
+        <button onClick={() => this.plus_10()}>+10</button>
+        <button onClick={() => this.plus_100()}>+100</button>
+        <button onClick={() => this.plus_1000()}>+1000</button>
+        <button onClick={() => this.reset()}>RESET</button>
+      </>
+    );
+  }
+}
+
+function Main(props) {
+  return (
+    <main>
+
+      <div className="Main">
+        <h1>Your can control Odometer here!</h1>
+      </div>
+      <Odometer />
+    </main>
+  );
+}
+
+function App() {
+
+  return (
+    <>
+      <Header text="Odometer Application" />
+      <Main />
+      <Footer trademark="Batool" />
+
+    </>
+  );
+}
+
 
 export default App;
